604a384165e5a62e7e37d4263e9f9a229bbf683a
mahsumyigit <ug.mahsum.yigit@toros.edu.tr>
2022-06-14 21:59:08 +0300

    add mocha

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,26 +1,25 @@
-const assert = require('assert');
-const ganache = require('ganache-cli');
-const Web3 = require('web3');
+const assert = require("assert");
+const ganache = require("ganache-cli");
+const Web3 = require("web3");
 const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require("../compile");
 
+let accounts;
+let inbox;
 
-class Car {
-    park() {
-        return 'stopped';
-    }
-    drive (){
-        return 'vroom';
-    }
-}
+beforeEach(async () => {
+  // Get a list of all accounts
+  accounts = await web3.eth.getAccounts();
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: ["Hi there!"],
+    })
+    .send({ from: accounts[0], gas: "1000000" });
+});
 
-describe('Car',()=>{
-    it('can park',()=>
-    {
-        const car = new Car();
-        assert.equal(car.park(),'stopped');
-    });
-    it ('can drive',()=>{
-        const car =new Car();
-        assert.equal(car.drive(),'vroom');
-    });
+describe("Inbox", () => {
+  it("deploys a contract", () => {
+    console.log(inbox);
+  });
 });
